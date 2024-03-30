b3f170418240695debd5f92d960819e8f6e61de0
jordigd20 <jordigd8@gmail.com>
2024-03-30 18:59:02 +0100

    feat: load boards from the state in the sidebar

--- src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -1,16 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarComponent } from './sidebar.component';
 import { Renderer2 } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NgClass } from '@angular/common';
 import Dexie, { PromiseExtended } from 'dexie';
+import { Board, Colors, IconType } from '../../../core/models/board.interface';
+import { BoardsSelectors, BoardsState } from '../../../boards/state';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
   let renderer: Renderer2;
+  let store: MockStore;
   let spyDbOpen: jest.SpyInstance<PromiseExtended<Dexie>>;
+
+  const mockBoards: Board[] = [
+    {
+      id: 1,
+      name: 'Default Board',
+      icon: IconType.Key,
+      color: Colors.Green,
+      tags: ['Concept'],
+      createdAt: new Date(),
+    },
+  ];
+
+  const initialState: BoardsState = {
+    boards: [],
+    selectedBoard: null,
+    status: 'pending',
+    error: null,
+  };
 
   beforeEach(async () => {
     spyDbOpen = jest
@@ -31,17 +52,44 @@ describe('SidebarComponent', () => {
             removeClass: jest.fn(),
           },
         },
+        provideMockStore({
+          initialState,
+          selectors: [
+            {
+              selector: BoardsSelectors.boards,
+              value: initialState.boards,
+            },
+            {
+              selector: BoardsSelectors.boardStatus,
+              value: initialState.status,
+            },
+          ],
+        }),
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     renderer = TestBed.inject(Renderer2);
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load the boards', () => {
+    expect(component.boards()).toEqual(initialState.boards);
+    expect(component.boardStatus()).toEqual(initialState.status);
+
+    store.overrideSelector(BoardsSelectors.boards, mockBoards);
+    store.overrideSelector(BoardsSelectors.boardStatus, 'success');
+    store.refreshState();
+    fixture.detectChanges();
+
+    expect(component.boards()).toEqual(mockBoards);
+    expect(component.boardStatus()).toEqual('success');
   });
 
   it('should toggle the theme mode in mobile view', () => {
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,16 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  OnInit,
   Renderer2,
   inject,
   signal,
 } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { BoardsActions, BoardsSelectors } from '../../../boards/state';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'sidebar',
@@ -19,12 +23,19 @@ import { RouterLink } from '@angular/router';
       'w-full max-h-screen py-4 px-0 lg:px-4  overflow-y-hidden overflow-x-hidden ',
   },
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   renderer = inject(Renderer2);
+  store = inject(Store);
 
   isDarkMode = signal<boolean>(
     document.documentElement.classList.contains('dark')
   );
+  boards = toSignal(this.store.select(BoardsSelectors.boards));
+  boardStatus = toSignal(this.store.select(BoardsSelectors.boardStatus));
+
+  ngOnInit() {
+    this.store.dispatch(BoardsActions.loadBoards());
+  }
 
   toggleThemeMode() {
     this.isDarkMode() ? this.switchToLightMode() : this.switchToDarkMode();
