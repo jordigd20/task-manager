d3833a2806f29cf73b444ac2229a749d4592ea32
jordigd20 <jordigd8@gmail.com>
2024-04-08 17:59:07 +0200

    feat: set the current page in the navigation sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,18 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  OnDestroy,
   OnInit,
   Renderer2,
   inject,
-  signal,
+  signal
 } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivationEnd, Router, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { BoardsActions, BoardsSelectors } from '../../../boards/state';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'sidebar',
@@ -19,22 +21,42 @@ import { toSignal } from '@angular/core/rxjs-interop';
   templateUrl: './sidebar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class:
-      'w-full max-h-screen py-4 px-0 xl:px-4  overflow-y-hidden overflow-x-hidden ',
-  },
+    class: 'w-full max-h-screen py-4 px-0 xl:px-4  overflow-y-hidden overflow-x-hidden '
+  }
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
   renderer = inject(Renderer2);
   store = inject(Store);
+  router = inject(Router);
 
-  isDarkMode = signal<boolean>(
-    document.documentElement.classList.contains('dark')
-  );
+  isDarkMode = signal<boolean>(document.documentElement.classList.contains('dark'));
   boards = toSignal(this.store.select(BoardsSelectors.boards));
   boardStatus = toSignal(this.store.select(BoardsSelectors.boardStatus));
+  activeBoard = signal<number | undefined>(undefined);
+  destroy$ = new Subject<void>();
+
+  constructor() {
+    this.router.events
+      .pipe(
+        filter((event: any) => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((event) => {
+        const idBoard = Number(event.snapshot.params['id']);
+
+        if (!isNaN(idBoard)) this.activeBoard.set(idBoard);
+        else this.activeBoard.set(undefined);
+      });
+  }
 
   ngOnInit() {
     this.store.dispatch(BoardsActions.loadBoards());
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   toggleThemeMode() {
